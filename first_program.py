8433497a868df35df707c289a7ab75a57942b5f8
Uppala  Dharani <118503248+Uppaladharani@users.noreply.github.com>
2024-08-08 13:17:03 +0530

    Update first_program.py

--- first_program.py
+++ first_program.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
-
 app = FastAPI()
 
-@app.get("/hello")
-async def hello():
-    return {"hello": "welcome"}
+@app.get("/")
+def first():
+    return {"hello": "here iam creating a docker file"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
 
